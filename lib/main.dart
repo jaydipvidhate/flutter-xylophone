47df8a71d9402b11bc50bbb3ba0ea5fcf6778bd9
RushikeshVidhate <www.rushikeshvidhate02@gmail.com>
2020-05-25 16:29:59 +0530

    Project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,52 @@
+
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+
 
 void main() => runApp(XylophoneApp());
 
+
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum){
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
+  Expanded buldKey({Color color,int soundNumber}){
+    return  Expanded(
+      child: FlatButton(onPressed: (){
+        playSound(soundNumber);
+      },
+      color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
-        ),
+        body:
+          Container(
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  buldKey(color: Colors.red[900],soundNumber: 1),
+                  buldKey(color: Colors.red[800],soundNumber: 2),
+                  buldKey(color: Colors.red[700],soundNumber: 3),
+                  buldKey(color: Colors.red[600],soundNumber: 4),
+                  buldKey(color: Colors.red,     soundNumber: 5),
+                  buldKey(color: Colors.red[400],soundNumber: 6),
+                  buldKey(color: Colors.red[300],soundNumber: 7),
+
+                ],
+              ),
+            )
+
+          ),
       ),
     );
   }
